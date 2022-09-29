7cdff58cfd16f122d0cddeeaeaee3f53a4d18a96
EarthmanAman <hashimathman@gmail.com>
2022-09-29 17:23:58 +0300

    Goal service

--- src/main/java/com/hashimathman/gms/service/GoalService.java
+++ src/main/java/com/hashimathman/gms/service/GoalService.java
@@ -1,4 +1,7 @@
 package com.hashimathman.gms.service;
 
+import com.hashimathman.gms.entity.Goal;
+
 public interface GoalService {
+    public Goal createGoal(Goal goal);
 }
---
+++ src/main/java/com/hashimathman/gms/service/GoalServiceImpl.java
@@ -0,0 +1,12 @@
+package com.hashimathman.gms.service;
+
+import com.hashimathman.gms.entity.Goal;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GoalServiceImpl implements GoalService {
+    @Override
+    public Goal createGoal(Goal goal) {
+        return null;
+    }
+}
